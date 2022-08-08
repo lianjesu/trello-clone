a6c06ac9de6faab15b557ca50d745d5a48ea2850
Lian Plasencia <lian.plasencia@ntsprint.com>
2022-08-08 08:47:47 -0400

    - Starting new repo

---
+++ src/styles.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const  AppContainer = styled.div`
+    align-items: flex-start;
+    background-color: #3179ba;
+    display: flex;
+    flex-direction: row;
+    height: 100%;
+    padding: 20px;
+    width: 100%;
+`
+export const ColumnContainer = styled.div`
+    background-color: #ebecf0;
+    width: 300%;
+    
+`
